879d4a6e7c4f09ca82aa03e50b1a1b84d080ed35
Kamilah Syahrabanu <kamilahsyahrabanu@gmail.com>
2024-11-20 02:30:30 +0700

    Not Found Page Added

--- frontend/src/config/routes.jsx
+++ frontend/src/config/routes.jsx
@@ -1,8 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/user/main/Home";
-import MainLayout from "../components/template/mainLayout";
+import MainLayout from "../components/template/MainLayout";
 import Login from "../pages/user/auth/login";
 import SignUp from "../pages/user/auth/SignUp";
+import NotFound from "../pages/user/main/NotFound";
 
 export const routes = createBrowserRouter([
   // AUTH
@@ -21,6 +22,10 @@ export const routes = createBrowserRouter([
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
       },
     ],
   },
