a582e332bbf17b1d58e41ebb3e092c9f8b5a15ab
alfiameilaniputri <alfiameilaniputri@gmail.com>
2024-11-22 02:25:52 +0700

    update file routes

--- frontend/src/config/routes.jsx
+++ frontend/src/config/routes.jsx
@@ -4,6 +4,8 @@ import Cart from "../pages/user/main/Cart";
 import MainLayout from "../components/template/MainLayout"; 
 import Login from "../pages/user/auth/Login";
 import SignUp from "../pages/user/auth/SignUp";
+import ForgotPassword from "../pages/user/auth/ForgotPassword";
+import ResetPassword from "../pages/user/auth/ResetPassword";
 import NotFound from "../pages/user/main/NotFound";
 import DetailProduct from "../pages/user/main/DetailProduct";
 import Checkout from "../pages/user/main/Checkout";
@@ -17,6 +19,14 @@ export const routes = createBrowserRouter([
   {
     path: "/signUp",
     element: <SignUp />,
+  },
+  {
+    path: "/forgot-password",
+    element: <ForgotPassword />,
+  },
+  {
+    path: "/reset-password",
+    element: <ResetPassword />,
   },
   // MAIN CONTENT
   {
