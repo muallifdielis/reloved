842d4b8263b9da9f132f8d58e5bca6ca33d01533
alfiameilaniputri <alfiameilaniputri@gmail.com>
2024-11-26 02:49:02 +0700

    Update file routes.jsx

--- frontend/src/config/routes.jsx
+++ frontend/src/config/routes.jsx
@@ -2,12 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../components/template/MainLayout";
 import SettingsLayout from "../components/template/SettingsLayout";
 import Home from "../pages/user/main/Home";
-import Login from "../pages/user/auth/login";
-import Cart from "../pages/user/main/Cart"; 
+import Login from "../pages/user/auth/Login";
+import Cart from "../pages/user/main/Cart";
 import SignUp from "../pages/user/auth/SignUp";
 import ForgotPassword from "../pages/user/auth/ForgotPassword";
 import ResetPassword from "../pages/user/auth/ResetPassword";
 import NotFound from "../pages/user/main/NotFound";
+import Products from "../pages/user/main/Products";
 import DetailProduct from "../pages/user/main/DetailProduct";
 import Checkout from "../pages/user/main/Checkout";
 import Shipping from "../pages/user/main/Shipping";
@@ -18,6 +19,10 @@ import Orders from "../pages/user/main/sell/Orders";
 import OrderDetail from "../pages/user/main/sell/OrderDetail";
 import Purchases from "../pages/user/main/buy/Purchases";
 import PurchaseDetail from "../pages/user/main/buy/PurchaseDetail";
+import SearchResults from "../pages/user/main/SearchResults";
+import AboutUs from "../pages/user/main/AboutUs";
+import AddProduct from "../pages/user/main/sell/AddProduct";
+import DetailPayment from "../pages/user/main/DetailPayment";
 
 export const routes = createBrowserRouter([
   // AUTH
@@ -46,12 +51,20 @@ export const routes = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "/cart", 
+        path: "/cart",
         element: <Cart />,
       },
       {
         path: "*",
         element: <NotFound />,
+      },
+      {
+        path: "/products",
+        element: <Products />,
+      },
+      {
+        path: "/search-results",
+        element: <SearchResults />,
       },
       {
         path: "/detail-product",
@@ -66,6 +79,11 @@ export const routes = createBrowserRouter([
         // path: "/shipping/:id",
         path: "/shipping",
         element: <Shipping />,
+      },
+      {
+        // path: "/shipping/detail-payment:id",
+        path: "shipping/detail-payment",
+        element: <DetailPayment />,
       },
       //USER PROFILE
       {
@@ -87,13 +105,17 @@ export const routes = createBrowserRouter([
         element: <Purchases />,
       },
       {
-        path: "/purchases",
-        element: <Purchases />,
-      },
-      {
         // path: "/purchases/detail/:id",
         path: "/purchases/detail",
         element: <PurchaseDetail />,
+      },
+      {
+        path: "/about-us",
+        element: <AboutUs />,
+      },
+      {
+        path: "/add-product",
+        element: <AddProduct />,
       },
     ],
   },
