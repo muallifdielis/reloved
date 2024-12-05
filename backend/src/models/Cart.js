1ae03b60c7e949c787848b3d9f908bfdd8ecf7ca
Dimas Fadillah <fdimas457@gmail.com>
2024-12-05 16:26:21 +0700

    Update Cart models

--- backend/src/models/Cart.js
+++ backend/src/models/Cart.js
@@ -1,49 +1,44 @@
 const mongoose = require("mongoose");
 
-const cartSchema = new mongoose.Schema(
-  {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: [true, "User is required"],
-    },
-    items: [
-      {
-        product: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Product",
-          required: [true, "Product is required"],
-        },
-        quantity: {
-          type: Number,
-          required: [true, "Quantity is required"],
-          min: [1, "Quantity must be at least 1"],
-        },
-        price: {
-          type: Number,
-          required: [true, "Price is required"],
-        },
-        total: {
-          type: Number,
-          required: true,
-        },
-      },
-    ],
-    totalPrice: {
-      type: Number,
-      required: true,
-      default: 0,
-    },
+const cartItemSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true
   },
-  {
-    timestamps: true,
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  total: {
+    type: Number,
+    required: true
   }
-);
-
-cartSchema.pre("save", function (next) {
-  this.totalPrice = this.items.reduce((sum, item) => sum + item.total, 0);
-  next();
 });
+
+const cartSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  items: [cartItemSchema],
+  totalPrice: {
+    type: Number,
+    default: 0
+  }
+});
+
+// Fungsi untuk menghitung total harga
+cartSchema.methods.calculateTotalPrice = function () {
+  this.totalPrice = this.items.reduce((total, item) => total + item.total, 0);
+  return this.totalPrice;
+};
 
 const Cart = mongoose.model("Cart", cartSchema);
 
