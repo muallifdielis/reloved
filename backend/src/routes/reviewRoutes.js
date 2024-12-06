98f61c1ef8b2d66225e36dc82f2abc911b95c050
Siti Nurlaela <ns793742@gmail.com>
2024-12-06 15:58:43 +0700

    update reviewRoutes

--- backend/src/routes/reviewRoutes.js
+++ backend/src/routes/reviewRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const reviewsController = require("../controllers/reviewsController");
-const verifyToken = require("../middleware/verifyToken");
+const reviewController = require("../controllers/reviewController");
+const { verifyToken } = require("../middleware/verifyToken");
 
 const reviewRoutes = express.Router();
 
-reviewRoutes.get("/", reviewsController.getAllReviews);
-reviewRoutes.post("/", verifyToken, reviewsController.createReview);
-reviewRoutes.delete("/:id", verifyToken, reviewsController.deleteReview);
+reviewRoutes.get("/", reviewController.getAllReviews);
+reviewRoutes.post("/", verifyToken, reviewController.createReview);
+reviewRoutes.put("/:id", verifyToken, reviewController.updateReview);
+reviewRoutes.delete("/:id", verifyToken, reviewController.deleteReview);
 
 module.exports = reviewRoutes;
