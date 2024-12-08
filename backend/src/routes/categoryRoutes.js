b7d0b1263feff7124d72ab36d6d6ab02dec97bdd
Siti Nurlaela <ns793742@gmail.com>
2024-12-08 20:01:25 +0700

    Update models, controller, and route category

--- backend/src/routes/categoryRoutes.js
+++ backend/src/routes/categoryRoutes.js
@@ -5,9 +5,9 @@ const categoryController = require("../controllers/categoryController");
 const categoryRoutes = express.Router(); 
 
 categoryRoutes.get("/", categoryController.getAllCategories);
-categoryRoutes.get("/:id", verifyToken, categoryController.getCategoryById);
-categoryRoutes.post("/", verifyToken, categoryController.createCategory);
-categoryRoutes.delete("/id", verifyToken, categoryController.deleteCategory);
-categoryRoutes.put("/:id", verifyToken, categoryController.updateCategory);
+categoryRoutes.get("/:id", verifyToken, isAdmin, categoryController.getCategoryById);
+categoryRoutes.post("/", verifyToken, isAdmin, categoryController.createCategory);
+categoryRoutes.delete("/id", verifyToken, isAdmin, categoryController.deleteCategory);
+categoryRoutes.put("/:id", verifyToken, isAdmin, categoryController.updateCategory);
 
 module.exports = categoryRoutes;
