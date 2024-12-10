a73f46453883a6f76a0bba19a3c13f3ae1d10434
Dimas Fadillah <fdimas457@gmail.com>
2024-12-10 17:17:34 +0700

    Update route

--- backend/src/routes/orderRoutes.js
+++ backend/src/routes/orderRoutes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const orderController = require("../controllers/orderController");
-const { verifyToken, isAdmin} = require("../middleware/verifyToken");
+const { verifyToken, isAdmin } = require("../middleware/verifyToken");
 
 const orderRoutes = express.Router();
 
 orderRoutes.post("/", verifyToken, orderController.createOrder);
 orderRoutes.get("/", verifyToken, orderController.getOrdersByUser);
 orderRoutes.get("/:id", verifyToken, orderController.getOrderById);
-orderRoutes.put("/:id/status", verifyToken, isAdmin, orderController.updateOrderStatus);
-orderRoutes.delete("/:id", verifyToken, orderController.deleteOrder);
+orderRoutes.put("/:id/status", verifyToken, orderController.updateOrderStatus);
+orderRoutes.delete("/:id", verifyToken, isAdmin, orderController.deleteOrder);
 
 module.exports = orderRoutes;
