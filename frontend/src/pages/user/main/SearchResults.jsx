fa542241e17c8c9b79ae986db3a4ccca18e18c3f
alfiameilaniputri <alfiameilaniputri@gmail.com>
2024-12-13 16:29:22 +0700

    Menambahkan function dan fetching untuk page Search Results

--- frontend/src/pages/user/main/SearchResults.jsx
+++ frontend/src/pages/user/main/SearchResults.jsx
@@ -1,24 +1,63 @@
 import { useEffect, useState } from "react";
-import {
-  Link,
-  useLocation,
-  useNavigate,
-  useSearchParams,
-} from "react-router-dom";
-import { FiChevronRight, FiChevronDown } from "react-icons/fi";
-import { BiSortAlt2 } from "react-icons/bi";
+import { Link, useLocation, useNavigate, useSearchParams } from "react-router-dom";
 import Card from "../../../components/common/Card";
+import LoadingSpinner from "../../../components/common/LoadingSpinner";
+import useProductStore from "../../../store/productStore";
+import useAuthStore from "../../../store/authStore";
 
 export default function SearchResults() {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isCategoryDropdownOpen, setIsCategoryDropdownOpen] = useState(false);
+  const [isSortDropdownOpen, setIsSortDropdownOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [products, setProducts] = useState([]);
   const location = useLocation();
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const query = searchParams.get("query");
+  const sortOption = searchParams.get("sort") || "newest";
+  const category = searchParams.get("category") || "semua";
 
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
+  const { currentUser } = useAuthStore(); 
+  const { searchProducts } = useProductStore();
+
+  const toggleCategoryDropdown = () => {
+    setIsCategoryDropdownOpen(!isCategoryDropdownOpen);
   };
+
+  const toggleSortDropdown = () => {
+    setIsSortDropdownOpen(!isSortDropdownOpen);
+  };
+
+  const handleSortChange = (option) => {
+    navigate(`/search?query=${query}&category=${category}&sort=${option}`);
+    setIsSortDropdownOpen(false);
+  };
+
+  const handleCategoryChange = (category) => {
+    navigate(`/search?query=${query}&category=${category}&sort=${sortOption}`);
+    setIsCategoryDropdownOpen(false);
+  };
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      setIsLoading(true);
+      if (query) {
+        try {
+          const response = await searchProducts(query, category, sortOption);
+          const filteredProducts = response.filter(
+            (product) => product?.seller?._id !== currentUser?._id
+          );
+          setProducts(filteredProducts || []);
+        } catch (error) {
+          console.error("Error fetching products:", error);
+        } finally {
+          setIsLoading(false);
+        }
+      }
+    };
+
+    fetchProducts();
+  }, [query, sortOption, category, searchProducts, currentUser?._id]);
 
   return (
     <div className="bg-white w-full min-h-screen">
@@ -39,90 +78,27 @@ export default function SearchResults() {
         </div>
       </section>
 
-      {/* Filter & Sort */}
-      <section className="flex justify-between px-10 py-6 md:px-12 lg:px-12 relative">
-        <div className="relative">
-          <button
-            onClick={toggleDropdown}
-            className={`flex items-center px-5 py-2 ${
-              location.search.includes("category")
-                ? "bg-primary ring-primary"
-                : "bg-white ring-secondary hover:bg-primary hover:ring-primary"
-            } text-black shadow-md rounded-full ring-2 hover:text-black text-sm`}
-          >
-            <span className="mr-2">Kategori</span>
-            <FiChevronDown />
-          </button>
-
-          {/* Dropdown */}
-          {isDropdownOpen && (
-            <div className="absolute left-0 z-30 mt-2 w-28 divide-y divide-gray-100 rounded-md border border-gray-100 bg-white shadow-lg">
-              <div className="p-2">
-                <button
-                  onClick={() => navigate("/products")}
-                  className={`block rounded-lg px-4 py-2 w-full text-sm text-gray-500 hover:bg-gray-50 hover:text-secondary ${
-                    location.search === "" && "bg-gray-50 text-secondary"
-                  }`}
-                >
-                  Semua
-                </button>
-                <button
-                  onClick={() => navigate("/products?category=men")}
-                  className={`block rounded-lg px-4 py-2 w-full text-sm text-gray-500 hover:bg-gray-50 hover:text-secondary ${
-                    location.search.includes("category=men") &&
-                    "bg-gray-50 text-secondary"
-                  }`}
-                >
-                  Pria
-                </button>
-                <button
-                  onClick={() => navigate("/products?category=women")}
-                  className={`block rounded-lg px-4 py-2 w-full text-sm text-gray-500 hover:bg-gray-50 hover:text-secondary ${
-                    location.search.includes("category=women") &&
-                    "bg-gray-50 text-secondary"
-                  }`}
-                >
-                  Wanita
-                </button>
-              </div>
-            </div>
-          )}
-        </div>
-
-        <button className="flex items-center px-5 py-2 bg-white text-black shadow-md rounded-full ring-2 ring-secondary hover:bg-primary hover:text-black hover:ring-primary text-sm">
-          <BiSortAlt2 className="mr-2" />
-          Sortir
-        </button>
-      </section>
-
       {/* Product Grid */}
       <section className="px-4 md:px-6 lg:px-8 pb-10">
-        <div className="grid grid-cols-1 gap-6 mx-auto justify-center md:grid-cols-2 lg:grid-cols-4 lg:gap-6 lg:px-0 lg:w-full">
-          <div className="w-full flex justify-center lg:scale-90">
-            <Card />
+        {isLoading ? (
+          <div className="w-full text-center py-10">
+            <LoadingSpinner />
           </div>
-          <div className="w-full flex justify-center lg:scale-90">
-            <Card />
+        ) : (
+          <div className="grid grid-cols-1 gap-6 mx-auto justify-center md:grid-cols-2 lg:grid-cols-4 lg:gap-6 lg:px-0 lg:w-full">
+            {products.length > 0 ? (
+              products.map((product) => (
+                <div key={product._id} className="w-full flex justify-center lg:scale-90">
+                  <Card product={product} />
+                </div>
+              ))
+            ) : (
+              <div className="w-full text-center col-span-4 text-gray-500">
+                Tidak ada produk yang ditemukan.
+              </div>
+            )}
           </div>
-          <div className="w-full flex justify-center lg:scale-90">
-            <Card />
-          </div>
-          <div className="w-full flex justify-center lg:scale-90">
-            <Card />
-          </div>
-          <div className="w-full flex justify-center lg:scale-90">
-            <Card />
-          </div>
-          <div className="w-full flex justify-center lg:scale-90">
-            <Card />
-          </div>
-          <div className="w-full flex justify-center lg:scale-90">
-            <Card />
-          </div>
-          <div className="w-full flex justify-center lg:scale-90">
-            <Card />
-          </div>
-        </div>
+        )}
       </section>
     </div>
   );
