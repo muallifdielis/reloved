449645a6dde4d6496e713a0f5f4f1549e1c49050
Dimas Fadillah <fdimas457@gmail.com>
2024-12-13 16:45:14 +0700

    User Model updated by adding 'isActive'

--- backend/src/models/Users.js
+++ backend/src/models/Users.js
@@ -56,6 +56,10 @@ const userSchema = new mongoose.Schema(
     ],
     isVerified: { type: Boolean, default: false },
     image: { type: String, default: "" },
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
   },
 
   {
