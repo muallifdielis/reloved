2bd2eefffc2c0fa2d6e2f51bed4b79b639d314cb
muallifdielis <alifdielis@gmail.com>
2024-12-15 20:31:18 +0700

    withdrawalRoutes update

--- backend/src/routes/withdrawalRoutes.js
+++ backend/src/routes/withdrawalRoutes.js
@@ -8,5 +8,5 @@ withdrawalRoutes.post("/", verifyToken, withdrawalController.createWithdrawal);
 withdrawalRoutes.get("/seller/earnings/:sellerId", verifyToken, withdrawalController.getSellerEarnings);
 withdrawalRoutes.get("/seller", verifyToken, withdrawalController.getWithdrawals);
 withdrawalRoutes.get("/:withdrawalId", verifyToken, withdrawalController.getWithdrawalById);
-
+withdrawalRoutes.delete("/:withdrawalId", verifyToken, withdrawalController.deleteWithdrawal);
 module.exports = withdrawalRoutes;
